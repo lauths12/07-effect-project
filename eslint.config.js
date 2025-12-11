674da7d4c9b5c9ea31b4f147d34576c2c2f4d77c
lauths12 <l.carretero@uniandes.edu.co>
2025-12-11 17:00:16 -0500

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: {
+      js,
+      "react-hooks": pluginReactHooks,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+
+  pluginReact.configs.flat.recommended,
+]);
